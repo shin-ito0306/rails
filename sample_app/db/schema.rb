b912fd75f23905f0e4fa4ad0a1ccdcfe6d47ede1
EC2 Default User <ec2-user@ip-172-31-44-142.ec2.internal>
2021-05-05 01:26:13 +0000

    [finish] 3-5

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_002953) do
+ActiveRecord::Schema.define(version: 2021_05_05_012220) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
